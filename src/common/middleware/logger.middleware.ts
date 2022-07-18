d6352d6ff1f07993debce66257882cd219667ad7
LeeGueTae <bigstar1201@naver.com>
2022-07-18 20:24:40 +0900

    Release V1.0.5

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -4,7 +4,13 @@ import { NextFunction, Request, Response } from "express";
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
    use(req: Request, res: Response, next: NextFunction) {
-      console.log("Request ...");
+      console.log(
+         "\n\nRequest ...",
+         "\n \\... Request Header :",
+         req.headers,
+         "\n \\... Request Body :",
+         req.body,
+      );
       next();
    }
 }
